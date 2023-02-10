02d4ea41b952d6ca68b9cf3573373aeb8b1bb5f6
Morganswayofficial <morganswayofficial@gmail.com>
2023-02-10 00:14:34 -0800

    Token

---
+++ personal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry piont
+ *
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 5; i++)
+	{
+		printf("%d ", i);
+	}
+	return (0);
+}
